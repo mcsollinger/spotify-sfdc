9e85d6fd0ed3a600d577661b868d8540c19c5f99
mcsollinger <miles.sollinger@codescience.com>
2024-10-14 22:04:24 -0400

    Refactor to use oauth authorization code grant

--- force-app/main/default/lwc/spotify/spotify.js
+++ force-app/main/default/lwc/spotify/spotify.js
@@ -1,52 +1,17 @@
 import { LightningElement } from 'lwc';
-import searchTrack from '@salesforce/apex/SpotifyController.searchTrack';
-import getAudioFeatures from '@salesforce/apex/SpotifyController.getTrackAudioFeatures';
 
 export default class Spotify extends LightningElement {
-    showSpinner = false;
-    trackQuery = '';
-    trackSearchResults;
-    selectedTrackId;
-    audioFeatures;
+    selectedItem = 'authorize';
 
-    get disableSearchBtn() {
-        return this.trackQuery.length === 0;
+    get showAuthorize() {
+        return this.selectedItem === 'authorize';
     }
 
-    handleTrackQueryChange(event) {
-        this.trackQuery = event.target.value;
+    get showTrackFeatures() {
+        return this.selectedItem === 'trackFeatures';
     }
 
-    handleSearchBtnClick() {
-        this.searchTrack();
-    }
-
-    async searchTrack() {
-        this.showSpinner = true;
-        try {
-            const searchResult = await searchTrack({ trackName: this.trackQuery });
-            this.trackSearchResults = JSON.parse(searchResult);
-        } catch (error) {
-            console.error(error);
-        }
-        this.showSpinner = false;
-    }
-
-    handleTrackClicked(event) {
-        this.selectedTrackId = event.detail.trackId;
-        console.log(this.selectedTrackId);
-        this.getSongFeatures();
-    }
-
-    async getSongFeatures() {
-        this.showSpinner = true;
-        try {
-            const audioFeaturesResult = await getAudioFeatures({ trackId: this.selectedTrackId });
-            this.audioFeatures = JSON.parse(audioFeaturesResult);
-            console.log(JSON.stringify(this.audioFeatures));
-        } catch (error) {
-            console.error(error);
-        }
-        this.showSpinner = false;
+    handleSelect(event) {
+        this.selectedItem = event.detail.name;
     }
 }
---
+++ force-app/main/default/lwc/spotifyTrackFeatures/spotifyTrackFeatures.js
@@ -0,0 +1,50 @@
+import { LightningElement } from 'lwc';
+import searchTrack from '@salesforce/apex/SpotifyController.searchTrack';
+import getAudioFeatures from '@salesforce/apex/SpotifyController.getTrackAudioFeatures';
+
+export default class SpotifyTrackFeatures extends LightningElement {
+    showSpinner = false;
+    trackQuery = '';
+    trackSearchResults;
+    selectedTrackId;
+    audioFeatures;
+
+    get disableSearchBtn() {
+        return this.trackQuery.length === 0;
+    }
+
+    handleTrackQueryChange(event) {
+        this.trackQuery = event.target.value;
+    }
+
+    handleSearchBtnClick() {
+        this.searchTrack();
+    }
+
+    async searchTrack() {
+        this.showSpinner = true;
+        try {
+            const searchResult = await searchTrack({ trackName: this.trackQuery });
+            this.trackSearchResults = JSON.parse(searchResult);
+        } catch (error) {
+            console.error(error);
+        }
+        this.showSpinner = false;
+    }
+
+    handleTrackClicked(event) {
+        this.selectedTrackId = event.detail.trackId;
+        this.getSongFeatures();
+    }
+
+    async getSongFeatures() {
+        this.showSpinner = true;
+        try {
+            const audioFeaturesResult = await getAudioFeatures({ trackId: this.selectedTrackId });
+            this.audioFeatures = JSON.parse(audioFeaturesResult);
+        } catch (error) {
+            console.error(error);
+        }
+        this.showSpinner = false;
+    }
+}
